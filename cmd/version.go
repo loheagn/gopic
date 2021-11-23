f771e3542b73a0b0b55283d49cfd94462dfa2e39
loheagn <loheagn@icloud.com>
2021-11-24 00:37:33 +0800

    Fix: version usage

--- cmd/version.go
+++ cmd/version.go
@@ -8,8 +8,8 @@ import (
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Check the version number of Gopic you used",
-	Long:  "Check the version number of Gopic you used",
+	Short: "Check the version number of Gopic you are using",
+	Long:  "Check the version number of Gopic you are using",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Gopic v0.01")
 	},
